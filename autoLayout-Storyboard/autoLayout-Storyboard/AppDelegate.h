1802774feaa13512304fb188e03ae4206053e855
krzyskopytek <krzys.kopytek@hotmail.com>
2016-05-10 16:55:39 -0400

    final

---
+++ autoLayout-Storyboard/autoLayout-Storyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  autoLayout-Storyboard
+//
+//  Created by Krzysztof Kopytek on 2016-05-10.
+//  Copyright © 2016 Krzysztof Kopytek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ autoLayout-Storyboard/autoLayout-Storyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  autoLayout-Storyboard
+//
+//  Created by Krzysztof Kopytek on 2016-05-10.
+//  Copyright © 2016 Krzysztof Kopytek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
